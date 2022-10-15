23670f6191358b8aa6be298c2e2b28f3d40addf7
nipaojala <nipa.ojala@gmail.com>
2022-10-15 14:11:54 +0300

    3.22 update

---
+++ controllers/person.js
@@ -0,0 +1,74 @@
+/* eslint-disable no-unused-vars */
+
+const personRouter = require('express').Router()
+const Person = require('./models/person')
+
+personRouter.get('/', (_req, res) => {
+  return res.redirect('/api/persons')
+})
+
+personRouter.get('/', (_req, res) => {
+  Person.find({}).then(persons => {
+    res.json(persons)
+  })
+})
+
+personRouter.get('/info', (_req, res) => {
+  Person.find({}).then(person => {
+    res.send(`Phonebook has info for ${person.length} people<br>${new Date()}`)
+  })
+
+})
+
+personRouter.get('/:id', (request, response, next) => {
+  Person.findById(request.params.id).then(person => {
+    if (person) {
+      response.json(person)
+    } else {
+      response.status(404).end()
+    }
+  })
+    .catch(error => next(error))
+})
+
+personRouter.delete('/:id', (request, response, next) => {
+  Person.findByIdAndRemove(request.params.id)
+    .then(_result => {
+      response.status(204).end()
+    })
+    .catch(error => next(error))
+})
+
+personRouter.post('/', (request, response, next) => {
+  const body = request.body
+
+  const person = new Person({
+    name: body.name,
+    number: body.number,
+  })
+  person.save().then(savedPerson => {
+    response.json(savedPerson)
+  })
+    .catch(error => next(error))
+})
+
+personRouter.put('/:id', (request, response, next) => {
+  const body = request.body
+  const person = {
+    name: body.name,
+    number: body.number,
+  }
+
+  Person.findByIdAndUpdate(request.params.id, person, { new: true, runValidators: true, context: 'query' })
+    .then(updatedPersonList => {
+      response.json(updatedPersonList)
+    })
+    .catch(error => next(error))
+})
+
+const PORT = process.env.PORT
+personRouter.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`)
+})
+
+module.exports = personRouter
--- models/person.js
+++ models/person.js
@@ -1,42 +1,33 @@
 /* eslint-disable no-unused-vars */
 const mongoose = require('mongoose')
 
-const url = process.env.MONGODB_URI
-console.log('connecting to', url)
-mongoose.connect(url)
-    .then(result => {
-        console.log('connected to MongoDB')
-    })
-    .catch((error) => {
-        console.log('error connecting to MongoDB:', error.message)
-    })
 const personSchema = new mongoose.Schema({
-    name: {
-        type: String,
-        required: true,
-        minlength: 3
-    },
-    number: {
-        minlength: 8,
-        type: String,
-        required: true,
-        validate: {
-            validator: function(v) {
-                return (/(0([1-9]{1,2})-([0-9]))|(0([0-9]{2})-([0-9]))/).test(v)
+  name: {
+    type: String,
+    required: true,
+    minlength: 3
+  },
+  number: {
+    minlength: 8,
+    type: String,
+    required: true,
+    validate: {
+      validator: function(v) {
+        return (/(0([1-9]{1,2})-([0-9]))|(0([0-9]{2})-([0-9]))/).test(v)
 
-            },
-            message: props => `${props.value} is not a valid phone number! Number must be
+      },
+      message: props => `${props.value} is not a valid phone number! Number must be
             formatted like XXX-XX... or XX-XX... totally 8 numbers atleast`
-        }
     }
+  }
 })
 
 personSchema.set('toJSON', {
-    transform: (document, returnedObject) => {
-        returnedObject.id = returnedObject._id.toString()
-        delete returnedObject._id
-        delete returnedObject.__v
-    }
+  transform: (document, returnedObject) => {
+    returnedObject.id = returnedObject._id.toString()
+    delete returnedObject._id
+    delete returnedObject.__v
+  }
 })
 
 module.exports = mongoose.model('Person', personSchema)
